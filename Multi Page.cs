fa77f0bb9727466bf72b95d55727990e2f3d241a
kavya84 <122438798+kavya84@users.noreply.github.com>
2024-02-18 23:12:26 -0600

    Create Multi Page.cs

---
+++ Multi Page.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MultiPage
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(DateTime.Now.Hour < 12)
+            {
+                LblWelcome.Text = "Good Morning";
+            }
+            else
+            {
+                LblWelcome.Text = "Good Night";
+            }
+        }
+    }
+}
